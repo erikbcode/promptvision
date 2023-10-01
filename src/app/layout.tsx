ac9f87b63ce9d369724c8a79f343fdd4d18cd116
Erik Buinevicius <erikbcode@gmail.com>
2023-10-01 01:02:49 -0500

    Code stylization fixes, added ToastContainer to display toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,22 @@
-import Providers from "@/components/Providers";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import Providers from '@/components/Providers';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "PromptVision",
-  description: "Created with Next.js 13, OpenAI, TailwindCSS, and more.",
+  title: 'PromptVision',
+  description: 'Created with Next.js 13, OpenAI, TailwindCSS, and more.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-zinc-50 dark:bg-zinc-900`}>
+        <ToastContainer />
         <Providers>{children}</Providers>
       </body>
     </html>
