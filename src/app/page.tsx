dc1a0fe493390c863f3be165c30a571f969c7f34
Erik Buinevicius <erikbcode@gmail.com>
2023-10-01 01:03:18 -0500

    Added dark theme styling for page header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import ImageGeneration from "@/components/ImageGeneration";
-import ThemeSelect from "@/components/ThemeSelect";
+import ImageGeneration from '@/components/ImageGeneration';
+import ThemeSelect from '@/components/ThemeSelect';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-8 p-24">
-      <header className="text-5xl italic font-bold">PromptVision</header>
+      <header className="text-5xl italic font-bold text-zinc-800 dark:text-zinc-200">PromptVision</header>
       <ThemeSelect />
       <ImageGeneration />
     </main>
