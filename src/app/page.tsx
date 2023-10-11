40f5fbf87e2da55750e3437d1c37d33634ee3912
Erik Buinevicius <erikbcode@gmail.com>
2023-10-11 14:45:09 -0500

    Added ImageGallery to main page and removed minimum height screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import ImageGallery from '@/components/ImageGallery';
 import ImageGeneration from '@/components/ImageGeneration';
 import ThemeSelect from '@/components/ThemeSelect';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-8 p-24">
+    <main className="flex flex-col items-center gap-8 p-24">
       <header className="text-5xl italic font-bold text-zinc-800 dark:text-zinc-200">PromptVision</header>
       <ThemeSelect />
       <ImageGeneration />
+      <ImageGallery />
     </main>
   );
 }
