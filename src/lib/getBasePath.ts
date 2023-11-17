d763fc8fc47cbce56f4971187c5b0ab061bfc12f
Erik Buinevicius <erikbcode@gmail.com>
2023-11-17 17:37:23 -0600

    Update getBasePath styling and remove console.log

--- src/lib/getBasePath.ts
+++ src/lib/getBasePath.ts
@@ -1,9 +1,6 @@
 export const getBasePath = () => {
   const base_url =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : `https://${process.env.VERCEL_URL}`;
+    process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : `https://${process.env.VERCEL_URL}`;
 
-  console.log("base_url: ", base_url);
   return base_url;
 };
