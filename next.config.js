fd9390f60acb41c6e91bb8d7e579e58393704455
Erik Buinevicius <erikbcode@gmail.com>
2023-11-17 17:36:50 -0600

    Add AWS_CLOUDFRONT_PREFIX env var to allowed image domains and configure Webpack to treat '@aws-sdk/signature-v4-multi-region' as an external dependency

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
-      }
+    domains: [
+      process.env.AWS_CLOUDFRONT_PREFIX,
     ]
-  }
+  },
+  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+    config.externals.push({
+        '@aws-sdk/signature-v4-multi-region': 'commonjs @aws-sdk/signature-v4-multi-region',
+    })
+
+    return config
+},
 }
 
 module.exports = nextConfig
